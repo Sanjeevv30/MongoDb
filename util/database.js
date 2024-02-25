a1dc1e6f1f585929a2b8abbe3bb8eb02292e7fba
Sanjeevv30 <sanjeevsrivastava107@gmail.com>
2024-02-25 11:56:00 +0530

    User-table

--- util/database.js
+++ util/database.js
@@ -1,15 +1,28 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+let _db;
+
 const MongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://sanjeevsrivastava107:rnjFm6wSScK6ZeXh@cluster0.g8m0xkj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://sanjeevsrivastava107:rnjFm6wSScK6ZeXh@cluster0.g8m0xkj.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
     .then((client) => {
       console.log("Connected!");
+      _db = client.db();
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
-module.exports = MongoConnect;
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+exports.MongoConnect = MongoConnect;
+exports.getDb = getDb;
